6586895f9e8017398ea93ad8db22f02b9bd7abba
GCHQ 11 <gchq.developer.11@gmail.com>
2019-04-23 23:26:04 +0100

    starting on video 3

---
+++ CORE_Programming/objectOriented/animals.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(object):
+    def __init__(self, species):
+        self.species = species
+
+    def get_species(self):
+        return self.species
+
+    @abstractmethod
+    def speak(self):
+        pass
+
+    def __str__(self):
+        return "Animal {}".format(self.species)
+
+    @staticmethod
+    def do_something():
+        return "What"
+
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__('Dog')
+
+    def speak(self):
+        print("Woof")
+
+
+class Cat(Animal):
+    def __init__(self):
+        super().__init__('Cat')
+
+    def speak(self):
+        print("Miaow")
+
+
+myAnimal = Animal('Rat')
+print(myAnimal)
+myAnimal.speak()
+
+myDog = Dog()
+print(myDog)
+myDog.speak()
+
+myCat = Cat()
+print(myCat)
+myCat.speak()
