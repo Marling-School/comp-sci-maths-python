4f1ca310b4b96af93f389cd80679ed791d8ac26a
GCHQ 11 <gchq.developer.11@gmail.com>
2019-04-28 23:25:52 +0100

    variable scope

--- CORE_Programming/practicalProgramming1/subroutines2_major_scales.py
+++ CORE_Programming/practicalProgramming1/subroutines2_major_scales.py
@@ -7,6 +7,26 @@ NOTE_NAMES: List[str] = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#
 MAJOR_SCALE_OFFSET: List[int] = [0, 2, 4, 5, 7, 9, 11, 12]
 
 
+def capture_user_note() -> str:
+    """
+    Capture the note that user wishes to see the scale for, it will loop until a valid selection is made.
+    :return: Name of the note as a string, it will be one of the notes in NOTE_NAMES
+    """
+    user_note: str = NOTE_NAMES[0]
+    while True:
+        print("The Known Musical Notes are")
+        for n in NOTE_NAMES:
+            print("\t{}".format(n), end = " ")
+        print("")
+        user_note = input("Please type in your selection: ")
+        if user_note in NOTE_NAMES:
+            print("Thanks")
+            break
+        else:
+            print("Invalid selection, have another go")
+    return user_note
+
+
 def get_major_scale_notes(starting_note: str) -> List[str]:
     notes: List[str] = []
     # Find the starting note, defaults to C if the given note is invalid
@@ -14,7 +34,7 @@ def get_major_scale_notes(starting_note: str) -> List[str]:
     try:
         starting_index: int = NOTE_NAMES.index(starting_note)
     except ValueError:
-        print("Invalid note given {}".format(starting_note))
+        print("Invalid note given '{}'".format(starting_note))
 
     for relative_offset in MAJOR_SCALE_OFFSET:
         offset_index: int = (starting_index + relative_offset) % len(NOTE_NAMES)
@@ -23,13 +43,9 @@ def get_major_scale_notes(starting_note: str) -> List[str]:
     return notes
 
 
-c_major = get_major_scale_notes("C")
-e_major = get_major_scale_notes("E")
-a_flat_major = get_major_scale_notes("G#/Ab")
-fish_major = get_major_scale_notes("fish")
+my_note: str = capture_user_note()
+major_scale = get_major_scale_notes(my_note)
+print("{} Major is {}".format(my_note, major_scale))
 
-print("C Major is {}".format(c_major))
-print("E Major is {}".format(e_major))
-print("Ab Major is {}".format(a_flat_major))
-print("fish Major is (should effectively default to C) {}".format(fish_major))
-
+invalid_major_scale = get_major_scale_notes("fish")
+print("Fish Major? {}".format(invalid_major_scale))
