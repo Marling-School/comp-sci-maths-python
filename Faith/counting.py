364747a0600a4a95e351d2ae69062046fcadf02b
GCHQ 11 <gchq.developer.11@gmail.com>
2019-05-13 22:00:57 +0100

    'faith'

---
+++ Faith/counting.py
@@ -0,0 +1,18 @@
+
+def calculatesAreaOfSquare(lengthOfSide):
+    return lengthOfSide * lengthOfSide
+
+
+def makeMeAPie(filling, radius):
+    print("Here is your pie with {} in it of radius {}".format(filling, radius))
+
+
+makeMeAPie('Chicken and mushroom', 20)
+
+
+
+# global difficulty
+# input weight
+# procedure setInitialDifficulty(weight)
+# 	    difficulty = weight DIV 6
+# endprocedure
