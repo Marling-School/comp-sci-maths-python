72e1f167944e52edb2a8649c2d142c6b024651c8
GCHQ 11 <gchq.developer.11@gmail.com>
2019-05-14 14:03:27 +0100

    most basic exeception handling

--- CORE_Programming/practicalProgramming1/most_basic_exception.py
+++ CORE_Programming/practicalProgramming1/most_basic_exception.py
@@ -7,3 +7,5 @@ try:
     print("It seems that you entered a valid number: {}".format(user_input_num))
 except ValueError:
     print("Apparently you did not enter a valid number: {}".format(user_input_str))
+
+print("But the show must go on!")
