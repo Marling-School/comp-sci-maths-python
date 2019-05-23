39372e64e1f18a920d8f21dc4c93d15ec200708a
GCHQ 11 <gchq.developer.11@gmail.com>
2019-05-23 23:01:12 +0100

    writing stack and queue and unit tests for both

---
+++ CORE_DataStructures/Stack/Stack.py
@@ -0,0 +1,40 @@
+from typing import List, TypeVar, Generic
+
+T = TypeVar('T')
+
+
+class Stack(Generic[T]):
+    capacity: int
+    data: List[T or None]
+    top: int
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.data = [None] * capacity
+        self.top = 0
+
+    def __str__(self):
+        return "{}".format(self.data)
+
+    def is_full(self):
+        return self.top == self.capacity
+
+    def is_empty(self):
+        return self.top == 0
+
+    def push(self, item: T):
+        if self.is_full():
+            raise Exception('Stack Overflow')
+
+        self.data[self.top] = item
+        self.top += 1
+
+    def peek(self) -> T:
+        if self.is_empty():
+            raise Exception('Stack Underflow')
+        return self.data[self.top - 1]
+
+    def pop(self) -> T:
+        item: T = self.peek()
+        self.top -= 1
+        return item
