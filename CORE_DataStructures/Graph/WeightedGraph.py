6dabc5711495531753867730c360acc28c79a834
Joe Sharp <joe.martin.sharp@gmail.com>
2019-05-24 08:21:45 +0100

    implementing weighted graph

---
+++ CORE_DataStructures/Graph/WeightedGraph.py
@@ -0,0 +1,41 @@
+from typing import Set, Dict, List, Callable, Tuple, TypeVar, Generic
+
+T = TypeVar('T', str, int)
+
+
+class WeightedGraph(Generic[T]):
+
+    __data: Dict[T, Set[Tuple[T, int]]]
+
+    def __init__(self):
+        self.__data = {}
+
+    def __repr__(self):
+        return "{}".format(self.__data)
+
+    def __ensure_key_exists(self, vertex: T):
+        if vertex not in self.__data:
+            self.__data[vertex] = set()
+
+    def add_relationship(self, from_v: T, to_v: T, weight: int):
+        self.__ensure_key_exists(from_v)
+        self.__ensure_key_exists(to_v)
+
+        self.__data[from_v].add((to_v, weight))
+
+    def get_relationships(self, from_v: T):
+        if from_v not in self.__data:
+            raise Exception("Graph Does Not Contain {}".format(from_v))
+
+        return self.__data[from_v]
+
+    def genrate_adjacency_matrix(self, to_index: Callable[[T], int]) -> List[List[T]]:
+        pass
+        return [[]]
+
+    def print_adj_list(self):
+        print("Adjacency List")
+        print("Key\tRelationships")
+        for key, relationships in self.__data.items():
+            print("{}\t{}".format(key, relationships))
+        print("-----")
---
+++ CORE_DataStructures/Graph/test.py
@@ -0,0 +1,15 @@
+import unittest
+from CORE_DataStructures.Graph.WeightedGraph import WeightedGraph
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_adj_list_graph(self):
+        my_graph: WeightedGraph[str] = WeightedGraph()
+        my_graph.add_relationship('J', 'K', 5)
+        my_graph.add_relationship('J', 'T', 3)
+        my_graph.add_relationship('T', 'I', 7)
+        print("My Graph:{}".format(my_graph))
+        my_graph.print_adj_list()
+        my_adj_matrix = my_graph.genrate_adjacency_matrix()
+        print("Adjacency Matrix", my_adj_matrix)
