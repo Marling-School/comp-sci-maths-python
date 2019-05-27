86255e84568a02a293b02654a8f1f07ab6b7d1f3
GCHQ 11 <gchq.developer.11@gmail.com>
2019-05-27 12:50:19 +0100

    person CSV should add a newline for each csv line

--- CORE_DataStructures/Files/Person.py
+++ CORE_DataStructures/Files/Person.py
@@ -1,7 +1,3 @@
-from __future__ import annotations
-from typing import List
-
-
 class Person:
     name: str
     age: int
@@ -13,14 +9,4 @@ class Person:
         self.favourite_colour = favourite_colour
 
     def to_csv(self) -> str:
-        return "{},{},{}".format(self.name, self.age, self.favourite_colour)
-
-    @staticmethod
-    def from_csv(csv: str) -> Person:
-        parts: List[str] = csv.split(",")
-        if len(parts) != 3:
-            raise Exception("Incorrect number of parts")
-        name: str = parts[0]
-        age: int = int(parts[1])
-        favourite_colour: str = parts[2]
-        return Person(name, age, favourite_colour)
+        return "{},{},{}\n".format(self.name, self.age, self.favourite_colour)
