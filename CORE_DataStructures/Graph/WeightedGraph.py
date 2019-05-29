b0a2d449b0e9ef9b590c86d4325e7c9232f8bdf7
GCHQ 11 <gchq.developer.11@gmail.com>
2019-05-29 14:52:39 +0100

    added adjacency matrix generation to weighted graph

--- CORE_DataStructures/Graph/WeightedGraph.py
+++ CORE_DataStructures/Graph/WeightedGraph.py
@@ -1,24 +1,25 @@
-from typing import Set, Dict, List, Callable, Tuple, TypeVar, Generic
-from functools import reduce
+from typing import Set, Dict, List, Tuple, TypeVar, Generic
+import math
 
-T = TypeVar('T', str, int)
+T = TypeVar('T', str, int, float)
 
 
 class WeightedGraph(Generic[T]):
 
-    __data: Dict[T, Set[Tuple[T, int]]]
+    __data: Dict[T, Set[Tuple[T, float]]]
+    __flat: List[T]
 
     def __init__(self):
         self.__data = {}
-
-    def __repr__(self):
-        return "{}".format(self.__data)
+        self.__flat = []
 
     def __ensure_key_exists(self, vertex: T):
         if vertex not in self.__data:
             self.__data[vertex] = set()
+        if vertex not in self.__flat:
+            self.__flat.append(vertex)
 
-    def add_relationship(self, from_v: T, to_v: T, weight: int):
+    def add_relationship(self, from_v: T, to_v: T, weight: float):
         self.__ensure_key_exists(from_v)
         self.__ensure_key_exists(to_v)
 
@@ -27,17 +28,33 @@ class WeightedGraph(Generic[T]):
     def get_relationships(self, from_v: T):
         if from_v not in self.__data:
             raise Exception("Graph Does Not Contain {}".format(from_v))
-
         return self.__data[from_v]
 
-    def generate_adjacency_matrix(self, to_index: Callable[[T], int]) -> List[List[T]]:
+    def get_relationship_weight(self, from_v: T, to_v: T):
+        from_data: Set[Tuple[T, float]] = self.__data[from_v]
 
-        pass
-        return [[]]
+        if from_data is not None:
+            for to_data in from_data:
+                if to_v is to_data[0]:
+                    return to_data[1]
 
-    def print_adj_list(self):
-        print("Adjacency List")
-        print("Key\tRelationships")
-        for key, relationships in self.__data.items():
-            print("{}\t{}".format(key, relationships))
-        print("-----")
+        return math.inf
+
+    def generate_adjacency_matrix(self) -> List[List[float]]:
+        matrix: List[List[float]] = [[math.inf for y in self.__flat] for x in self.__flat]
+
+        for key, edges in self.__data.items():
+            key_index: int = self.__flat.index(key)
+            for edge in edges:
+                dest_index: int = self.__flat.index(edge[0])
+                weight: float = edge[1]
+                matrix[key_index][dest_index] = weight
+
+        return matrix
+
+    def get_known_items(self) -> List[T]:
+        return self.__flat
+
+    def __repr__(self):
+        return "{}".format(self.__data)
+
--- CORE_DataStructures/Graph/test.py
+++ CORE_DataStructures/Graph/test.py
@@ -10,6 +10,23 @@ class TestStringMethods(unittest.TestCase):
         my_graph.add_relationship('J', 'T', 3)
         my_graph.add_relationship('T', 'I', 7)
         print("My Graph:{}".format(my_graph))
-        my_graph.print_adj_list()
-        my_adj_matrix = my_graph.generate_adjacency_matrix(ord)
+        my_adj_matrix = my_graph.generate_adjacency_matrix()
         print("Adjacency Matrix", my_adj_matrix)
+
+        known_items = my_graph.get_known_items()
+        print("-", end="\t")
+        for heading in known_items:
+            print("{}".format(heading), end="\t")
+        print("|")
+        for outer_index, outer in enumerate(known_items):
+            print("{}".format(outer), end="\t")
+            for inner_index, inner in enumerate(known_items):
+                print("{}".format(my_adj_matrix[outer_index][inner_index]), end="\t")
+            print("|")
+
+        j_k: float = my_graph.get_relationship_weight('J', 'K')
+        self.assertEqual(5.0, j_k)
+        j_t: float = my_graph.get_relationship_weight('J', 'T')
+        self.assertEqual(3.0, j_t)
+        t_i: float = my_graph.get_relationship_weight('T', 'I')
+        self.assertEqual(7.0, t_i)
