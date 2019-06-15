16a602a0855a988fbf1e42984b030ce57a22cf33
GCHQ 11 <gchq.developer.11@gmail.com>
2019-06-15 15:23:27 +0100

    refining test structure for arithmetic expressions

--- CORE_Algorithms/ArithmeticExpression/test.py
+++ CORE_Algorithms/ArithmeticExpression/test.py
@@ -1,31 +1,39 @@
 import unittest
+from typing import List, Tuple, Dict
 from CORE_Algorithms.ArithmeticExpression.ArithmeticExpression import ArithmeticExpression
 from CORE_Algorithms.ReversePolishNotation.ReversePolishNotation import ReversePolishNotation
 
 
 class TestArithmeticExpression(unittest.TestCase):
-    def test(self):
-        my_expression: ArithmeticExpression = ArithmeticExpression("((A+B)*(C+D))")
-        print("My Expression: {}".format(my_expression))
+
+    def __general_test(self,
+                       expressions: List[str],
+                       cases: List[Tuple[Dict[str, float], float]]):
+        for e in expressions:
+            my_expression: ArithmeticExpression = ArithmeticExpression(e)
+            print("My Expression: {}".format(my_expression))
+            rpn: ReversePolishNotation = ReversePolishNotation(my_expression.postfix())
+
+            for inputs, expected in cases:
+                ans1: float = rpn.evaluate(inputs)
+                self.assertEqual(expected, ans1)
 
     def test_complex_1(self):
-        my_expression: ArithmeticExpression = ArithmeticExpression("((A + B) * (C - D))")
-        print("My Expression: {}".format(my_expression))
-        rpn: ReversePolishNotation = ReversePolishNotation(my_expression.postfix())
-
-        ans1: float = rpn.evaluate({"A": 9, "B": 2, "C": 15, "D": 7})
-        self.assertEqual((9 + 2) * (15 - 7), ans1)
-
-        ans2: float = rpn.evaluate({"A": 91, "B": 12, "C": 7, "D": 45})
-        self.assertEqual((91 + 12) * (7 - 45), ans2)
+        self.__general_test(
+            # An expression in 3 forms
+            ["((A + B) * (C - D))", "(*(+AB)(-CD))", "((AB+)(CD-)*)"],
+            [
+                # Dictionary of inputs, paired with expected answer
+                ({"A": 9, "B": 2, "C": 15, "D": 7}, (9 + 2) * (15 - 7)),
+                ({"A": 91, "B": 12, "C": 7, "D": 45}, (91 + 12) * (7 - 45))
+            ]
+        )
 
     def test_complex_2(self):
-        my_expression: ArithmeticExpression = ArithmeticExpression("((B/(C+D))*A)")
-        print("My Expression: {}".format(my_expression))
-        rpn: ReversePolishNotation = ReversePolishNotation(my_expression.postfix())
-
-        ans1: float = rpn.evaluate({"A": 45, "B": 3, "C": 6, "D": 17})
-        self.assertEqual((3 / (6 + 17)) * 45, ans1)
-
-        ans2: float = rpn.evaluate({"A": 13, "B": 4, "C": 61, "D": 2})
-        self.assertEqual((4 / (61 + 2)) * 13, ans2)
+        self.__general_test(
+            ["((B/(C+D))*A)", "(*(/B(+CD))A)", "((B(CD+)/)A*)"],
+            [
+                ({"A": 45, "B": 3, "C": 6, "D": 17}, (3 / (6 + 17)) * 45),
+                ({"A": 13, "B": 4, "C": 61, "D": 2}, (4 / (61 + 2)) * 13)
+            ]
+        )
