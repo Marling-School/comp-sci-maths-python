32d8e3f3bb0a8d89fc47b1f45aba838e8cb0e6a1
Joe Sharp <joe.martin.sharp@gmail.com>
2019-06-21 14:05:40 +0100

    set notation

--- CORE_TheoryOfComputation/SetDemo/intersection.py
+++ CORE_TheoryOfComputation/SetDemo/intersection.py
@@ -3,9 +3,10 @@ from CORE_TheoryOfComputation.SetDemo.capture_csv import capture_string_csv
 
 first: Set[str] = capture_string_csv('Please type the first list')
 second: Set[str] = capture_string_csv('Please type the second subset')
+intersect: Set[str] = first.intersection(second)
 
-print("Intersection of {} and {}: {}".format(
+print("Intersection of \n{} and \n{}:\n\t{}".format(
     first,
     second,
-    first.intersection(second)
+    "\n\t".join([str(x) for x in intersect])
 ))
--- CORE_TheoryOfComputation/SetDemo/union.py
+++ CORE_TheoryOfComputation/SetDemo/union.py
@@ -3,9 +3,11 @@ from CORE_TheoryOfComputation.SetDemo.capture_csv import capture_string_csv
 
 first: Set[str] = capture_string_csv('Please type the first list')
 second: Set[str] = capture_string_csv('Please type the second subset')
+union: Set[str] = first.union(second)
 
-print("Union of {} and {}: {}".format(
+print("Union of \n{} and \n{}:\n\t{}".format(
     first,
     second,
-    first.union(second)
+    "\n\t".join([str(x) for x in union])
 ))
+
