98adc82523358b90bb3dd7097f3d4868451899ad
Joe Sharp <joe.martin.sharp@gmail.com>
2019-06-26 11:33:58 +0100

    commenting

--- CORE_DataRepresentation/test.py
+++ CORE_DataRepresentation/test.py
@@ -15,12 +15,17 @@ class Test(TestCase):
         This test checks that the decryption successfully recovers the data.
         :param key: The key to use
         :param test_cases: A list of strings to encrypt and decrypt
-        :return:
         """
+
+        # Create a cipher with this key
         my_cipher: VernamCipher = VernamCipher(key)
         for plain_text in test_cases:
+
+            # Call upon the cipher to encrypt then decrypt that plain text
             cipher_text: str = my_cipher.encrypt(plain_text)
             recovered_plain_text: str = my_cipher.decrypt(cipher_text)
+
+            # Does the recovered plain text match the given plain text?
             self.assertEqual(plain_text.lower(), recovered_plain_text)
 
     def test_caesar(self):
@@ -32,15 +37,26 @@ class Test(TestCase):
                                     ["hello", "it was the best of times, it was the worst of times"])
 
     def __test_break_vernam(self, key: List[int], test_cases: List[str]):
+        """
+        General form of a brute force attack test.
+        :param key: The key to use
+        :param test_cases: Strings to encrypt and then attempt decrypt
+        """
+        # Create a cipher for this key
+        my_cipher: VernamCipher = VernamCipher(key)
+
         for plain_text in test_cases:
-            my_cipher: VernamCipher = VernamCipher(key)
+            # Call upon the cipher to encrypt our string
             my_cipher_text: str = my_cipher.encrypt(plain_text)
+
+            # Attempt to find a key
             found_key, score = get_keys(my_cipher_text, len(key), WORDS_FILENAME)
+
+            # Try to decrypt with that found key
             my_found_cipher: VernamCipher = VernamCipher(found_key)
             my_found_plain_text: str = my_found_cipher.decrypt(my_cipher_text)
-            print('Brute force decrypt found {} and used it to decrypt\n{}\ninto\n{}'
-                  .format(found_key, my_cipher_text, my_found_plain_text))
-            self.assertEqual(1.0, score)
+
+            # Does the found plain text match the give plain text?
             self.assertEqual(plain_text.lower(), my_found_plain_text)
 
     def test_brute_force_caesar(self):
