ac61aa54a5a976217eb887772987efc8454ee2d5
GCHQ 11 <gchq.developer.11@gmail.com>
2019-07-08 23:43:07 +0100

    imperiod

--- CORE_Programming/concepts1/my_tuples.py
+++ CORE_Programming/concepts1/my_tuples.py
@@ -1,37 +1,37 @@
 from typing import List
 from collections import namedtuple
 
-nameAndScore = namedtuple("nameAndScore", "name score")
+NameAndScore = namedtuple("nameAndScore", "name score")
 
 
-def getNameAndScore(rawCsv: str) -> nameAndScore:
-    parts: List[str] = rawCsv.split(",")
+def get_name_and_score(raw_csv: str) -> NameAndScore:
+    parts: List[str] = raw_csv.split(",")
     name: str = parts[0]
     score: int = int(parts[1])
-    return nameAndScore(name, score)
+    return NameAndScore(name, score)
 
 
-answer = getNameAndScore("Lucy,50")
+answer = get_name_and_score("Lucy,50")
 print("The Name is {} with a score of {}".format(answer.name, answer.score))
 
 
 # Parses a CSV such as 'Fred,10' into a tuple containing the name and the score
-def getNameAndScoreAgain(rawCsv: str) -> (str, int):
-    parts: List[str] = rawCsv.split(",")
+def get_name_and_score_again(raw_csv: str) -> (str, int):
+    parts: List[str] = raw_csv.split(",")
     name: str = parts[0]
     score: int = int(parts[1])
     return name, score
 
 
-name1, score1 = getNameAndScoreAgain("Fred,45")
+name1, score1 = get_name_and_score_again("Fred,45")
 print("1 - Name is {} and Score is {}".format(name1, score1))
 
-nameScore2 = getNameAndScoreAgain("Sally,104")
+nameScore2 = get_name_and_score_again("Sally,104")
 print("2 - Name is {} and Score is {}".format(nameScore2[0], nameScore2[1]))
 
 
-def getFloorAndMod(a: int, b: int) -> (int, int):
+def get_floor_and_mod(a: int, b: int) -> (int, int):
     return a // b, a % b
 
 
-print(getFloorAndMod(77, 13))
+print(get_floor_and_mod(77, 13))
