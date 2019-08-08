718d771460ce3495b17ad19affc22e0531cd1395
Joe Sharp <joe.martin.sharp@gmail.com>
2019-08-08 15:28:50 +0100

    returning functions from functions

--- CORE_FunctionalProgramming/sorting_custom_order.py
+++ CORE_FunctionalProgramming/sorting_custom_order.py
@@ -4,6 +4,20 @@ from random import randint
 T = TypeVar('T')
 
 
+def get_sorting_algorithm(name: str) -> Callable[[T, T], bool]:
+    """
+    Retrieve a sorting algorithm from one of the named ones
+    :param name:
+    :return:
+    """
+    if name == "ascending":
+        return lambda x, y: x < y
+    elif name == "descending":
+        return lambda x, y: x > y
+    else:
+        raise Exception(f"Unknown sorting algorithm: {name}")
+
+
 def bubble_sort(items: List[T], in_right_order: Callable[[T, T], bool]) -> List[T]:
     """
     Sort a list of items, using a custom comparator
@@ -25,8 +39,11 @@ def bubble_sort(items: List[T], in_right_order: Callable[[T, T], bool]) -> List[
 my_list: List[int] = [randint(0, 100) for x in range(20)]
 
 # Sort it using two different comparison functions
-ascending_list: List[int] = bubble_sort(my_list, lambda x, y: x < y)
-descending_list: List[int] = bubble_sort(my_list, lambda x, y: x > y)
+sort_asc = get_sorting_algorithm("ascending")
+ascending_list: List[int] = bubble_sort(my_list, sort_asc)
+
+sort_desc = get_sorting_algorithm("descending")
+descending_list: List[int] = bubble_sort(my_list, sort_desc)
 
 print(f"Raw: {my_list}")
 print(f"Ascending: {ascending_list}")
